faa565e0ac502a05d25bf5aa1e67a0058dad38cb
soundslikebliss <russianid1@gmail.com>
2014-09-21 05:57:52 +0000

    css animation for header and steps, background color changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+	$('#header_circle').addClass('animated bounceInLeft');
+	$('#step1').addClass('animated rollIn');
+	
